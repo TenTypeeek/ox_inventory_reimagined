272b2c588a40aafecee34a74e092fcb61c03634d
TenTypeeek <marek.mcs120@gmail.com>
2025-05-24 13:17:42 +0200

    data
    data\items.lua
    data\shops.lua

--- data/items.lua
+++ data/items.lua
@@ -1,61 +1,70 @@
 return {
-	['testburger'] = {
-		label = 'Test Burger',
-		weight = 220,
-		degrade = 60,
+	-- FOOD & DRINK
+	['burger'] = {
+		label = 'Burger',
+		weight = 250, -- typical fast food burger
 		client = {
-			image = 'burger_chicken.png',
+			label = 'You are eating a burger',
 			status = { hunger = 200000 },
 			anim = 'eating',
 			prop = 'burger',
 			usetime = 2500,
-			export = 'ox_inventory_examples.testburger'
-		},
-		server = {
-			export = 'ox_inventory_examples.testburger',
-			test = 'what an amazingly delicious burger, amirite?'
-		},
-		buttons = {
-			{
-				label = 'Lick it',
-				action = function(slot)
-					print('You licked the burger')
-				end
-			},
-			{
-				label = 'Squeeze it',
-				action = function(slot)
-					print('You squeezed the burger :(')
-				end
-			},
-			{
-				label = 'What do you call a vegan burger?',
-				group = 'Hamburger Puns',
-				action = function(slot)
-					print('A misteak.')
-				end
-			},
-			{
-				label = 'What do frogs like to eat with their hamburgers?',
-				group = 'Hamburger Puns',
-				action = function(slot)
-					print('French flies.')
-				end
-			},
-			{
-				label = 'Why were the burger and fries running?',
-				group = 'Hamburger Puns',
-				action = function(slot)
-					print('Because they\'re fast food.')
-				end
-			}
-		},
-		consume = 0.3
+		}
 	},
 
+	['mustard'] = {
+		label = 'Mustard',
+		weight = 400,
+		client = {
+			label = 'You are drinking mustard',
+			status = { hunger = 25000, thirst = 25000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `prop_food_mustard`, pos = vec3(0.01, 0.0, -0.07), rot = vec3(1.0, 1.0, -1.5) },
+			usetime = 2500,
+		}
+	},
+
+	['ketchup'] = {
+		label = 'Ketchup',
+		weight = 400,
+		client = {
+			label = 'You are drinking ketchup',
+			status = { hunger = 25000, thirst = 25000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `prop_food_mustard`, pos = vec3(0.01, 0.0, -0.07), rot = vec3(1.0, 1.0, -1.5) },
+			usetime = 2500,
+		}
+	},
+
+	['sprunk'] = {
+		label = 'Sprunk',
+		weight = 355,
+		client = {
+			label = 'You are drinking sprunk',
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `prop_ld_can_01`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
+			usetime = 2500,
+		}
+	},
+
+	['water'] = {
+		label = 'Water',
+		weight = 500,
+		client = {
+			label = 'You are drinking water',
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `prop_ld_flow_bottle`, pos = vec3(0.03, 0.03, 0.02), rot = vec3(0.0, 0.0, -1.5) },
+			usetime = 2500,
+			cancel = true,
+		}
+	},
+
+	-- MEDICAL
 	['bandage'] = {
 		label = 'Bandage',
-		weight = 115,
+		weight = 100,
 		client = {
 			anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
 			prop = { model = `prop_rolled_sock_02`, pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
@@ -64,83 +73,41 @@ return {
 		}
 	},
 
-	['black_money'] = {
-		label = 'Dirty Money',
+	-- CONSUMABLES (MISC)
+	['panties'] = {
+		label = 'Panties',
+		weight = 50,
 	},
 
-	['burger'] = {
-		label = 'Burger',
-		weight = 220,
-		client = {
-			status = { hunger = 200000 },
-			anim = 'eating',
-			prop = 'burger',
-			usetime = 2500,
-			notification = 'You ate a delicious burger'
-		},
-	},
+	-- UTILITY ITEMS
 
-	['sprunk'] = {
-		label = 'Sprunk',
-		weight = 350,
-		client = {
-			status = { thirst = 200000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_ld_can_01`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
-			usetime = 2500,
-			notification = 'You quenched your thirst with a sprunk'
-		}
+	['lockpick'] = {
+		label = 'Lockpick',
+		weight = 100,
 	},
 
 	['parachute'] = {
 		label = 'Parachute',
-		weight = 8000,
+		weight = 5000,
 		stack = false,
 		client = {
+			label = 'You are putting on a parachute',
 			anim = { dict = 'clothingshirt', clip = 'try_shirt_positive_d' },
 			usetime = 1500
 		}
 	},
 
-	['garbage'] = {
-		label = 'Garbage',
-	},
-
 	['paperbag'] = {
 		label = 'Paper Bag',
-		weight = 1,
+		weight = 5,
 		stack = false,
 		close = false,
 		consume = 0
 	},
 
-	['identification'] = {
-		label = 'Identification',
-		client = {
-			image = 'card_id.png'
-		}
-	},
-
-	['panties'] = {
-		label = 'Knickers',
-		weight = 10,
-		consume = 0,
-		client = {
-			status = { thirst = -100000, stress = -25000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_cs_panties_02`, pos = vec3(0.03, 0.0, 0.02), rot = vec3(0.0, -13.5, -1.5) },
-			usetime = 2500,
-		}
-	},
-
-	['lockpick'] = {
-		label = 'Lockpick',
-		weight = 160,
-	},
-
 	['phone'] = {
 		label = 'Phone',
-		weight = 190,
+		weight = 250,
 		stack = false,
 		consume = 0,
 		client = {
@@ -149,7 +116,6 @@ return {
 					pcall(function() return exports.npwd:setPhoneDisabled(false) end)
 				end
 			end,
-
 			remove = function(total)
 				if total < 1 then
 					pcall(function() return exports.npwd:setPhoneDisabled(true) end)
@@ -158,68 +124,46 @@ return {
 		}
 	},
 
-	['money'] = {
-		label = 'Money',
-	},
-
-	['mustard'] = {
-		label = 'Mustard',
-		weight = 500,
-		client = {
-			status = { hunger = 25000, thirst = 25000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_food_mustard`, pos = vec3(0.01, 0.0, -0.07), rot = vec3(1.0, 1.0, -1.5) },
-			usetime = 2500,
-			notification = 'You.. drank mustard'
-		}
-	},
-
-	['water'] = {
-		label = 'Water',
-		weight = 500,
-		client = {
-			status = { thirst = 200000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_ld_flow_bottle`, pos = vec3(0.03, 0.03, 0.02), rot = vec3(0.0, 0.0, -1.5) },
-			usetime = 2500,
-			cancel = true,
-			notification = 'You drank some refreshing water'
-		}
-	},
-
 	['radio'] = {
-		label = 'Radio',
-		weight = 1000,
+		label = 'Walkie Talkie',
+		weight = 1200,
 		stack = false,
 		allowArmed = true
 	},
 
-	['armour'] = {
-		label = 'Bulletproof Vest',
-		weight = 3000,
-		stack = false,
-		client = {
-			anim = { dict = 'clothingshirt', clip = 'try_shirt_positive_d' },
-			usetime = 3500
-		}
+	-- DOCUMENTS & CARDS
+	['identification'] = {
+		label = 'ID Card',
+		weight = 5,
 	},
 
-	['clothing'] = {
-		label = 'Clothing',
-		consume = 0,
-	},
-
-	['mastercard'] = {
+	['fleecacard'] = {
 		label = 'Fleeca Card',
 		stack = false,
-		weight = 10,
-		client = {
-			image = 'card_bank.png'
-		}
+		weight = 5,
+	},
+
+	-- VALUABLES & CURRENCY
+	['money'] = {
+		label = 'Money',
+		weight = 1,
+	},
+
+	['black_money'] = {
+		label = 'Dirty Money',
+		weight = 1,
+	},
+
+	-- JUNK / MISCELLANEOUS
+	['garbage'] = {
+		label = 'Garbage',
+		weight = 300,
 	},
 
 	['scrapmetal'] = {
 		label = 'Scrap Metal',
-		weight = 80,
+		weight = 250,
 	},
+
+	-- ADDON STUFF HERE
 }
--- data/shops.lua
+++ data/shops.lua
@@ -19,16 +19,66 @@ return {
 			vec3(2557.94, 382.05, 108.62),
 			vec3(373.55, 325.56, 103.56),
 		}, targets = {
-			{ loc = vec3(25.06, -1347.32, 29.5), length = 0.7, width = 0.5, heading = 0.0, minZ = 29.5, maxZ = 29.9, distance = 1.5 },
-			{ loc = vec3(-3039.18, 585.13, 7.91), length = 0.6, width = 0.5, heading = 15.0, minZ = 7.91, maxZ = 8.31, distance = 1.5 },
-			{ loc = vec3(-3242.2, 1000.58, 12.83), length = 0.6, width = 0.6, heading = 175.0, minZ = 12.83, maxZ = 13.23, distance = 1.5 },
-			{ loc = vec3(1728.39, 6414.95, 35.04), length = 0.6, width = 0.6, heading = 65.0, minZ = 35.04, maxZ = 35.44, distance = 1.5 },
-			{ loc = vec3(1698.37, 4923.43, 42.06), length = 0.5, width = 0.5, heading = 235.0, minZ = 42.06, maxZ = 42.46, distance = 1.5 },
-			{ loc = vec3(1960.54, 3740.28, 32.34), length = 0.6, width = 0.5, heading = 120.0, minZ = 32.34, maxZ = 32.74, distance = 1.5 },
-			{ loc = vec3(548.5, 2671.25, 42.16), length = 0.6, width = 0.5, heading = 10.0, minZ = 42.16, maxZ = 42.56, distance = 1.5 },
-			{ loc = vec3(2678.29, 3279.94, 55.24), length = 0.6, width = 0.5, heading = 330.0, minZ = 55.24, maxZ = 55.64, distance = 1.5 },
-			{ loc = vec3(2557.19, 381.4, 108.62), length = 0.6, width = 0.5, heading = 0.0, minZ = 108.62, maxZ = 109.02, distance = 1.5 },
-			{ loc = vec3(373.13, 326.29, 103.57), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(24.434, -1347.878, 28.497),
+                heading = 270,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(-3038.384, 584.742, 6.908),
+                heading = 20,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(-3241.695, 999.855, 11.830),
+                heading = 0,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(1727.602, 6414.655, 34.037),
+                heading = 245,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(1697.376, 4923.442, 41.063),
+                heading = 320,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(1960.319, 3739.504, 31.343),
+                heading = 290,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(549.059, 2671.894, 41.156),
+                heading = 90,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(2678.577, 3279.136, 54.241),
+                heading = 325,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(2557.793, 380.818, 107.623),
+                heading = 0,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(372.355, 325.909, 102.566),
+                heading = 260,
+            },
 		}
 	},
 
@@ -49,12 +99,36 @@ return {
 			vec3(1392.562, 3604.684, 34.980),
 			vec3(-1393.409, -606.624, 30.319)
 		}, targets = {
-			{ loc = vec3(1134.9, -982.34, 46.41), length = 0.5, width = 0.5, heading = 96.0, minZ = 46.4, maxZ = 46.8, distance = 1.5 },
-			{ loc = vec3(-1222.33, -907.82, 12.43), length = 0.6, width = 0.5, heading = 32.7, minZ = 12.3, maxZ = 12.7, distance = 1.5 },
-			{ loc = vec3(-1486.67, -378.46, 40.26), length = 0.6, width = 0.5, heading = 133.77, minZ = 40.1, maxZ = 40.5, distance = 1.5 },
-			{ loc = vec3(-2967.0, 390.9, 15.14), length = 0.7, width = 0.5, heading = 85.23, minZ = 15.0, maxZ = 15.4, distance = 1.5 },
-			{ loc = vec3(1165.95, 2710.20, 38.26), length = 0.6, width = 0.5, heading = 178.84, minZ = 38.1, maxZ = 38.5, distance = 1.5 },
-			{ loc = vec3(1393.0, 3605.95, 35.11), length = 0.6, width = 0.6, heading = 200.0, minZ = 35.0, maxZ = 35.4, distance = 1.5 }
+			            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(1134.284, -983.057, 45.415),
+                heading = 280,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(-1221.482, -908.059, 11.326),
+                heading = 35,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(-1486.693, -377.557, 39.163),
+                heading = 135,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(-2966.382, 391.487, 14.043),
+                heading = 80,
+            },
+            {
+                ped = `mp_m_shopkeep_01`,
+                scenario = 'WORLD_HUMAN_AA_COFFEE',
+                loc = vec3(1165.177, 2710.794, 37.157),
+                heading = 185,
+            },
 		}
 	},
 
